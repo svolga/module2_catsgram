2a05fd5c8bc1e51a1562879d88275de05725d62a
Ракчеев Сергей <rakcheev.sg@magnat.ru>
2023-08-22 10:13:07 +0300

    password selector

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.catsgram.service.HackCatService;
 
+import java.util.Optional;
+
 @RestController
 public class SimpleController {
 
@@ -18,9 +20,12 @@ public class SimpleController {
     // внедрите бин HackCatService
 
     @GetMapping("/do-hack")
-    public void doHack(){
-        // хакните этих котиков
-        hackCatService.doHackNow();
+    public Optional<String> doHack(){
+
+        return hackCatService.doHackNow()
+                .map(password -> "Ура! Пароль подобран: " + password)
+                .or(() -> Optional.of("Не удалось подобрать пароль. "
+                        + " Проверьте состояние и настройки базы данных."));
     }
 
     @GetMapping("/home")
